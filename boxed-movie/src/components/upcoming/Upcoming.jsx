dbee50535a48c2535dceeba291295ef0a711a9ba
habibashi <habibashi187@gmail.com>
2022-12-30 12:03:48 +0200

    fetch up coming movies from api

--- boxed-movie/src/components/upcoming/Upcoming.jsx
+++ boxed-movie/src/components/upcoming/Upcoming.jsx
@@ -1,10 +1,51 @@
-import classes from './Upcoming.module.css';
+import { useState, useEffect } from 'react';
+import Card from '../card/Card';
+import classes from '../top-Rated/TopRated.module.css';
 
 const Upcoming = () => {
+    const [shows, setShows] = useState([]);
+
+    useEffect(() => {
+        const getTopRated = async () => {
+            const response = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=87c5413076a3b5fe9972da817ec29abe");
+            if (!response.ok) {
+                throw new Error('request Failed!');
+            }
+            const data = await response.json();
+
+            const loadedShows = [];
+            for (const movie of data.results) {
+                loadedShows.push({
+                    id: movie.id,
+                    name: movie.title.slice(0, 13),
+                    date: movie.release_date,
+                    rate: movie.vote_average,
+                    vote: movie.vote_count,
+                    image: movie.poster_path
+                });
+            };
+            setShows(loadedShows);
+        }
+        getTopRated();
+    }, []);
+
     return (
         <div className="pt-1 px-4">
-            <div className="rounded-2 p-2" id={classes.background}>
+            <div className="rounded-2 p-2" id={classes.banner}>
                 <span className='p-2' id={classes.text}><i style={{ color: 'aqua' }} className="bi bi-fast-forward-fill"></i> Upcoming</span>
+            </div>
+            <div id={classes.card} className='d-flex my-3 py-3'>
+                {shows.map((show) => (
+                    <Card
+                        key={show.id}
+                        id={show.id}
+                        name={show.name}
+                        rate={show.rate}
+                        vote={show.vote}
+                        image={show.image}
+                        date={show.date}
+                    />
+                ))}
             </div>
         </div>
     )
