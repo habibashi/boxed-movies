23134ce3bc013b8e5dd9e40d5b74475336e360e0
habibashi <habibashi187@gmail.com>
2022-12-30 03:59:50 +0200

    put main and navbar in app component

---
+++ boxed-movie/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/navBar/NavBar';
+import Main from "./components/main/Main";
+function App() {
+  return (
+    <>
+      <div className='container-fluid m-0 p-0'>
+        <NavBar />
+        <Main />
+      </div>
+    </>
+  )
+}
+
+export default App;
