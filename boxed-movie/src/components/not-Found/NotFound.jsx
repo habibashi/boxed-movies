85a3ae0b3f58543d48baf8f74fcca0d7b7fe5c38
Habibashi <96149736+habibashi@users.noreply.github.com>
2023-01-01 21:48:29 +0200

    not found route

---
+++ boxed-movie/src/components/not-Found/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <div className="d-flex align-items-center justify-content-center mt-5">
+            <div>
+                <h1 style={{ color: "white" }}>404 page not found</h1>;
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
